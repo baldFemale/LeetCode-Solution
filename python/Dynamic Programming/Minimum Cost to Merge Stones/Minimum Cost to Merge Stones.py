1b32fa8ab9f107f71056ff8d7d4ef5153f985004
baldFemale <zjuzjj@gmail.com>
2019-08-26 13:16:35 +0800

    add 1000

---
+++ python/Dynamic Programming/Minimum Cost to Merge Stones/Minimum Cost to Merge Stones.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mergeStones(self, stones: List[int], K: int) -> int:
+        N = len(stones)
+        prefix = [0 for i in range(N + 1)]
+        for i in range(N):
+            prefix[i + 1] += (prefix[i] + stones[i])
+
+        inf = float("inf")
+
+        import functools
+
+        @functools.lru_cache(None)
+        def dp(i, j, m):
+            if (j - i + 1 - m) % (K - 1) != 0:
+                return inf
+            if i == j:
+                return inf if m != 1 else 0
+            if m == 1:
+                return dp(i, j, K) + prefix[j + 1] - prefix[i]
+
+            return min(dp(i, mid, 1) + dp(mid + 1, j, m - 1) for mid in range(i, j))
+
+        res = dp(0, N - 1, 1)
+        return res if res != inf else -1
